d9f2c2544b7bc755705e570a7ac8fba2dfa6ea29
MaxInMoon <maxlamiable@gmail.com>
2024-08-26 16:17:25 +0200

    Update CartSlice.ts

--- src/features/CartSlice.ts
+++ src/features/CartSlice.ts
@@ -16,14 +16,44 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action: PayloadAction<CartItem>) => {
-      if (state.items.find((item) => item.id === action.payload.id)) {
+      const exisitingItemIndex =
+        state.items.findIndex((item) => item.id === action.payload.id) || 0;
+
+      if (exisitingItemIndex) {
+        state.items[exisitingItemIndex].quantity += action.payload.quantity;
       } else {
         state.items.push(action.payload);
       }
     },
 
+    // addToCart: (state, action: PayloadAction<CartItem>) => {
+    //   const exisitingItem = state.items.find(
+    //     (item) => item.id === action.payload.id
+    //   );
+
+    //   if (exisitingItem) {
+    //     state.items = state.items.map((item) => {
+    //       if (item.id === action.payload.id) {
+    //         item.quantity += action.payload.quantity;
+    //       }
+    //       return item;
+    //     });
+    //   } else {
+    //     state.items.push(action.payload);
+    //   }
+    // },
+
+    // addToCart: (state, action: PayloadAction<CartItem>) => {
+    //   if (state.items.find((item) => item.id === action.payload.id)) {
+    //   } else {
+    //     state.items.push(action.payload);
+    //   }
+    // },
+
     removeFromCart: (state, action: PayloadAction<CartItem>) => {
-      state.items = state.items.filter((item) => item.name !== action.payload.name);
+      state.items = state.items.filter(
+        (item) => item.name !== action.payload.name
+      );
     },
   },
 });
