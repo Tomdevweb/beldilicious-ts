09f93c42ca28b9605596a62f3f90e8017805e3a6
MaxInMoon <maxlamiable@gmail.com>
2024-08-26 16:50:48 +0200

    Fix react-hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,6 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
-import { fixupPluginRules } from "@eslint/compat";
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
@@ -12,7 +11,7 @@ export default [
   pluginReact.configs.flat.recommended,
   {
     plugins: {
-      "react-hooks": fixupPluginRules(pluginReactHooks),
+      "react-hooks": pluginReactHooks,
     },
     rules: pluginReactHooks.configs.recommended.rules,
   },
