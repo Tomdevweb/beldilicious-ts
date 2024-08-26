6ef1ffef3dec50c5dcab228bf6ce24641ce93b98
Tomdevweb <thomasalpa.contact@gmail.com>
2024-08-26 16:43:49 +0200

    eslint react-hooks config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,8 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import { fixupPluginRules } from "@eslint/compat";
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
@@ -10,6 +11,15 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
-    rules: { "@typescript-eslint/ban-ts-comment": "off" },
+    plugins: {
+      "react-hooks": fixupPluginRules(pluginReactHooks),
+    },
+    rules: pluginReactHooks.configs.recommended.rules,
+  },
+  {
+    rules: {
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
   },
 ];
